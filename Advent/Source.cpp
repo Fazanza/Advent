ad5d93c317dfac88814e7b1ae4ac7fa3abf69841
wmyke <wmyke@TABLET-ADHCPOVO>
2022-12-02 19:25:26 -0500

    Add project files.

---
+++ Advent/Source.cpp
@@ -0,0 +1,17 @@
+#include <fstream>
+
+
+using namespace std;
+
+int main() {
+    int curMaximum = 0;
+    int curValue = 0;
+
+    ifstream ins;
+    ins.open("input.txt");
+    while(getline(ins, curValue)){
+
+    }
+    
+    return 0;
+}
